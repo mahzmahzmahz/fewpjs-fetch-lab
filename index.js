ff6220eddd53881483d8d29680049468999aa7f8
Alex Marz <alexhmarz@gmail.com>
2021-02-05 18:09:09 -0500

    commit tree 036804c0e64e101f8d20beed8cc749db85a611e1

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 
+  
 }
 
 function renderBooks(books) {
